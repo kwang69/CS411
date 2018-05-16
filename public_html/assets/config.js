8fbfe23fd47e08bfcc04c690228b3839aaa002db
WK196 <wangkuang@WKdeMacBook-Pro.local>
2018-05-16 15:57:51 +0800

    done

---
+++ public_html/assets/config.js
@@ -0,0 +1,34 @@
+// cpanel - site_templates/countdown_fresh/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        email: "",
+        logo: "Housematch",
+        social: [
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+    },
+    slides: [
+        {
+            type: 'countdown',
+            backgroundImage: "\/home\/housematch\/housematch.jpg",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            endTime: '2018-03-26T17:30:00.000Z',
+            content: "",
+        }
+    ]
+};
